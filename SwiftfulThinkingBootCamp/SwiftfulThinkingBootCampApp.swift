51520a75044ca32afc9bbe7b07e48dcce761657f
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-25 20:54:42 +0300

    Initial Commit

---
+++ SwiftfulThinkingBootCamp/SwiftfulThinkingBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootCampApp.swift
+//  SwiftfulThinkingBootCamp
+//
+//  Created by Amby on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
