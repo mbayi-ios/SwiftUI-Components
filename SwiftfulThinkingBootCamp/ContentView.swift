9322ecfbd48aa133ba9f347b471dc4da61ad7537
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-29 09:22:06 +0300

    using stacks git add .

--- SwiftfulThinkingBootCamp/ContentView.swift
+++ SwiftfulThinkingBootCamp/ContentView.swift
@@ -9,12 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .font(.title)
-            .fontWeight(.bold)
-            .foregroundColor(Color.blue)
-            .multilineTextAlignment(.center)
-            .padding()
+        VStack {
+            Text("VStack Item 1")
+            Text("VStack Item 2")
+            Spacer()
+            Divider()
+            Text("VStack Item 3")
+
+            HStack {
+                Text("Item 1")
+                Divider().background(Color.black)
+                Text("HStack Item 2")
+                Divider()
+                    .background(Color.black)
+                Spacer()
+                Text("HStack Item 3")
+            }
+            .background(Color.red)
+            ZStack{
+                Text("ZStack Item 1")
+                    .padding()
+                    .background(Color.green)
+                    .opacity(0.8)
+                Text("Zstack item 2")
+                    .padding()
+                    .background(Color.green)
+                    .offset(x: 80, y: -400)
+            }
+        }
+        .background(Color.blue)
     }
 }
 
